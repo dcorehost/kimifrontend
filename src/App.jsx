85553b1d64021444e337970df63ab9398d75420c
Rahul jha <jhar46646@gmail.com>
2025-01-06 17:35:22 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,54 +2,64 @@
 
 
 
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar';
-import Specialize from './Components/Specialize/Specialize';
-import Footer from './Components/Footer/Footer';
-import StatsAndFAQ from './Components/StatsAndFAQ/StatsAndFAQ';
-import ReadyToStart from './Components/ReadyToStart/ReadyToStart';
-import LogoCarousel from './Components/LogoCarousel/LogoCarousel';
-import WhatIsKimi from './Components/WhatIsKimi/WhatIsKimi';
-import WelcomePage from './Components/WelcomePage/WelcomePage';
+// import React from 'react';
+// import './App.css';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Navbar from './Components/Navbar/Navbar';
+// import Specialize from './Components/Specialize/Specialize';
+// import Footer from './Components/Footer/Footer';
+// import StatsAndFAQ from './Components/StatsAndFAQ/StatsAndFAQ';
+// import ReadyToStart from './Components/ReadyToStart/ReadyToStart';
+// import LogoCarousel from './Components/LogoCarousel/LogoCarousel';
+// import WhatIsKimi from './Components/WhatIsKimi/WhatIsKimi';
+// import WelcomePage from './Components/WelcomePage/WelcomePage';
+// import Homepage from './Pages/Homepage/Homepage';
+// import MetaAds from './Pages/MetaAds/MetaAds';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <WelcomePage></WelcomePage>
+//       <LogoCarousel />
+//       <WhatIsKimi />
+//       <Specialize />
+//       <StatsAndFAQ />
+//       <ReadyToStart />
+//       <Footer />
+      
+//       <Routes>
+//          {/* <Route path='/ '  element={<Homepage />}></Route>
+//          <Route path='/meta-ads'  element={<MetaAds />}></Route> */}
+//       </Routes>
+     
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage/Homepage";
+import MetaAds from "./Pages/MetaAds/MetaAds";
+
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <WelcomePage></WelcomePage>
-      <LogoCarousel />
-      <Specialize />
-      <StatsAndFAQ />
-      <ReadyToStart />
-      {/* <WhatIsKimi /> */}
-      <Footer />
-      
       <Routes>
-         {/* <Route path='/performance-management'  element={<PerformanceManagement />}></Route> */}
+        {/* Define routes for your pages */}
+        <Route path="/" element={<Homepage />} />
+        <Route path="/meta-ads" element={<MetaAds />} />
+       
       </Routes>
-     
     </Router>
   );
 };
 
 export default App;
-
-
-
-
-
-
-// import { Routes, Route } from 'react-router-dom';
-// import Homepage from './Pages/Homepage/Homepage';
-
-// const App = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Homepage />} />
-//     </Routes>
-//   );
-// };
-
-// export default App;
---
+++ src/Pages/MetaAds/MetaAds.jsx
@@ -0,0 +1,17 @@
+import Footer from "../../Components/Footer/Footer";
+import Navbar from "../../Components/Navbar/Navbar";
+
+
+
+const MetaAds = () => {
+  return (
+    <>
+    <Navbar />
+    <Footer />
+
+     
+    </>
+  );
+};
+
+export default MetaAds;
