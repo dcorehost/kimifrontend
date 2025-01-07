37a301a878f45b45d0b0255638e08fc72babfdba
Rahul jha <jhar46646@gmail.com>
2025-01-07 18:10:06 +0530

    meta ads bing ads google ads contact us

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,15 @@
+import Footer from "../../Components/Footer/Footer";
 import Login from "../../Components/Login/Login";
+import Navbar from "../../Components/Navbar/Navbar";
 
 
 
 const LoginPage = () => {
   return (
     <>
+    <Navbar />
    <Login />
+   <Footer />
    
      
     </>
