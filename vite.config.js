f59185da26971e1150b5b53ff7be8d2742f5b3f0
Deepika <dcorehost2@gmail.com>
2025-02-14 13:56:57 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/kimi',
   server: {
     host: true, // or '0.0.0.0' to bind to all interfaces
   },
